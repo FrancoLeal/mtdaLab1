7b719d79df5469d158aeae1281424affd51e4ce6
Nicolas <nicolas.romero.f@usach.cl>
2019-08-29 15:04:48 -0400

    boxplot

--- lab1.r
+++ lab1.r
@@ -1,14 +1,21 @@
 require('mclust')
+require('reshape2')
 
-#path = "F:/Google Drive/USACH/Nivel 8/Analisis de datos/lab3/hepatitis.data"
-path = "~/Documentos/2-2019/TMDA/Lab 1/breast-cancer-wisconsin.data"
-
+#path = "~/Documentos/2-2019/TMDA/Lab 1/breast-cancer-wisconsin.data
+path = "~/Escritorio/USACH/Topicos/Taller de mineria de datos avanzada/tmdaLab1/breast-cancer-wisconsin.data"
 data =  read.table(path,sep=",", na.strings = c("?"))
 
 names = c('ID','Clump Thickness','Uniformity of Cell Size',
           'Uniformity of Cell Shape','Marginal Adhesion','Single Epithelial Cell Size',
           'Bare Nuclei','Bland Chromatin','Normal Nucleoli','Mitoses','Class')
 colnames(data) = names
-show(data)
 
 summary(data)
+
+
+dat.m = melt(data, id=c('ID','Class'))
+
+library(ggplot2)
+p = ggplot(dat.m) + geom_boxplot(aes(x=variable, y=value)) + facet_grid(.~Class)
+
+print(p)

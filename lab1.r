1f232397344de3cc8e890d1daa2984b4a429e3f1
Franco <franco.leal.@usach.cl>
2019-09-02 22:42:01 -0400

    clusters y correlacion

--- lab1.r
+++ lab1.r
@@ -1,7 +1,8 @@
 require('mclust')
 require('ggplot2')
 require('reshape2')
-
+require('Hmisc')
+require('corrplot')
 path = "~/Documentos/2-2019/TMDA/Lab 1/breast-cancer-wisconsin.data"
 #path = "~/Escritorio/USACH/Topicos/Taller de mineria de datos avanzada/tmdaLab1/breast-cancer-wisconsin.data"
 data =  read.table(path,sep=",", na.strings = c("?"))
@@ -20,6 +21,8 @@ medians <- sapply(data.without.na,median)
 modes <- sapply(data.without.na,mode)
 vars <- sapply(data.without.na,var)
 
+show(sqrt(vars)/means)
+
 
 dat.m = melt(data.without.na, id=c('ID','Class'))
 p = ggplot(dat.m) + geom_boxplot(aes(x=variable, y=value)) + facet_grid(.~Class)
@@ -35,14 +38,28 @@ summary(BIC)
 
 show(vars)
 
-data.3 = subset(data.2, select=-M)
+modelo = Mclust(data.2[,1:9],x=BIC)
+summary(modelo)
+plot(modelo,what = "classification")
 
-BIC=mclustBIC(data.3[,1:8], prior = priorControl(functionName="defaultPrior", shrinkage=0.1))
-plot(BIC)
+
+corr = rcorr(as.matrix(data.2))
+show(corr)
+
+data.3 = subset(data.2,select=-UCSize)
+
+BIC2=mclustBIC(data.3[,1:8], prior = priorControl(functionName="defaultPrior", shrinkage=0.1))
+plot(BIC2)
 
 summary(BIC)
 
+show(vars)
+
 modelo = Mclust(data.3[,1:8],x=BIC)
 summary(modelo)
-plot(modelo,what = "classification")
+plot(modelo,what = "density")
+
+m = cor(data.3)
+corrplot(m,method="number")
+
 #coef de variacion desv est/media

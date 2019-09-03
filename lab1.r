21d7e4bb8cc471c1b51c95586b852b78e8c156fe
Franco <franco.leal.@usach.cl>
2019-09-03 00:21:53 -0400

    comentarios

--- lab1.r
+++ lab1.r
@@ -10,8 +10,8 @@ getmode <- function(x){
   uniqv[which.max(tabulate(match(x,uniqv)))]
 }
 
-#path = "~/Documentos/2-2019/TMDA/Lab 1/breast-cancer-wisconsin.data"
-path = "~/Escritorio/USACH/Topicos/Taller de mineria de datos avanzada/tmdaLab1/breast-cancer-wisconsin.data"
+path = "~/Documentos/2-2019/TMDA/tmdaLab1/breast-cancer-wisconsin.data"
+#path = "~/Escritorio/USACH/Topicos/Taller de mineria de datos avanzada/tmdaLab1/breast-cancer-wisconsin.data"
 data =  read.table(path,sep=",", na.strings = c("?"))
 
 names = c('ID','shape','size',
@@ -30,45 +30,65 @@ data.2 = data.without.na[,2:10]
 
 #Cálculo de medidas de tendencia y variación
 means <- sapply(data.2,mean)
+means
 # Medias
-#       CT   UCSize  UCShape       MA     SECS       BN       BC       NN        M 
-# 4.442167 3.150805 3.215227 2.830161 3.234261 3.544656 3.445095 2.869693 1.603221 
+#    shape     size  shape.1  adhesion epithelial  nuclei chromatin   nucleoli    mitoses  
+# 4.442167 3.150805 3.215227 2.830161   3.234261 3.544656 3.445095   2.869693    1.603221 
 medians <- sapply(data.2,median)
 # Medianas
-# CT  UCSize UCShape      MA    SECS      BN      BC      NN       M 
-#  4       1       1       1       2       1       3       1       1 
+# shape     size shape.1  adhesion epithelial   nuclei  chromatin   nucleoli    mitoses 
+#     4        1       1         1         2         1          3        1           1 
 modes <-sapply(data.2,getmode)
 # Modas
-# CT  UCSize UCShape      MA    SECS      BN      BC      NN       M 
-#  1       1       1       1       2       1       3       1       1 
+# shape    size shape.1 adhesion epithelial   nuclei  chromatin   nucleoli    mitoses 
+#     1       1       1       1       2           1         3         1             1 
 vars <- sapply(data.2,var)
 # Varianzas
-#       CT    UCSize   UCShape        MA      SECS        BN        BC        NN 
-# 7.956694  9.395113  8.931615  8.205717  4.942109 13.277695  6.001013  9.318772 
-#        M 
-# 3.002160 
+#   shape       size   shape.1  adhesion epithelial   nuclei  chromatin nucleoli    mitoses
+# 7.956694  9.395113  8.931615  8.205717  4.942109 13.277695  6.001013  9.318772   3.002160 
 
 show(sqrt(vars)/means)
 
 #Coeficientes de variación
-#       CT    UCSize   UCShape        MA      SECS        BN        BC        NN 
-# 0.6349967 0.9728132 0.9295085 1.0121552 0.6873551 1.0279861 0.7110679 1.0637608 
-#         M 
-# 1.0807456
+#     shape     size    shape.1 adhesion  epithelial   nuclei chromatin  nucleoli    mitoses
+# 0.6349967 0.9728132 0.9295085 1.0121552 0.6873551 1.0279861 0.7110679 1.0637608  1.0807456
 
 data.benigno <- data.without.na[which(data.without.na$Class == "Benigno"),]
 data.maligno <- data.without.na[which(data.without.na$Class == "Maligno"),]
 
 means.benigno <- sapply(data.benigno,mean)
+#        shape         size        shape     adhesion   epithelial       nuclei    chromatin 
+# 2.963964e+00 1.306306e+00 1.414414e+00 1.346847e+00 2.108108e+00 1.346847e+00 2.083333e+00 
+#     nucleoli      mitoses 
+# 1.261261e+00 1.065315e+00
 medians.benigno <- sapply(data.benigno,median)
-modes.benigno <- sapply(data.benigno,mode)
+# shape       size      shape   adhesion epithelial     nuclei  chromatin   nucleoli    mitoses 
+#     3          1          1          1          2          1          2          1          1 
+modes.benigno <- sapply(data.benigno,getmode)
+# shape       size      shape   adhesion epithelial     nuclei  chromatin   nucleoli    mitoses 
+#   "1"        "1"        "1"        "1"        "2"        "1"        "2"        "1"        "1" 
 vars.benigno <- sapply(data.benigno,var)
+#        shape         size        shape     adhesion   epithelial       nuclei    chromatin 
+# 2.797796e+00 7.321498e-01 9.159091e-01 8.410510e-01 7.693246e-01 1.387326e+00 1.128480e+00 
+#     nucleoli      mitoses 
+# 9.112724e-01 2.598326e-01
 
 means.maligno <- sapply(data.maligno,mean)
+#        shape         size        shape     adhesion   epithelial       nuclei    chromatin 
+# 7.188285e+00 6.577406e+00 6.560669e+00 5.585774e+00 5.326360e+00 7.627615e+00 5.974895e+00 
+#     nucleoli      mitoses 
+# 5.857741e+00 2.602510e+00
 medians.maligno <- sapply(data.maligno,median)
-modes.maligno <- sapply(data.maligno,mode)
+# shape       size      shape   adhesion epithelial     nuclei  chromatin   nucleoli    mitoses 
+#   "8"        "6"        "6"        "5"        "5"       "10"        "7"        "6"        "1" 
+modes.maligno <- sapply(data.maligno,getmode)
+# shape       size      shape   adhesion epithelial     nuclei  chromatin   nucleoli    mitoses 
+#  "10"       "10"       "10"       "10"        "3"       "10"        "7"       "10"        "1" 
 vars.maligno <- sapply(data.maligno,var)
-
+#       shape         size        shape     adhesion   epithelial       nuclei    chromatin 
+# 5.943392e+00 7.421504e+00 6.600295e+00 1.021845e+01 5.968672e+00 9.713688e+00 5.209451e+00 
+#     nucleoli      mitoses
+# 1.121497e+01 6.576632e+00
 
 dat.m = melt(data.without.na, id=c('ID','Class'))
 p = ggplot(dat.m) + geom_boxplot(aes(x=variable, y=value)) + facet_grid(.~Class)
@@ -101,24 +121,24 @@ summary(modelo)
 # Clustering table:
 #  1   2   3   4 
 #115 274 235  59 
-plot(modelo,what = "classification")
+plot(modelo,whatx = "classification")
 
 
 corr = rcorr(as.matrix(data.2))
 show(corr)
 
-#           CT UCSize UCShape   MA SECS   BN   BC   NN    M
-# CT      1.00   0.64    0.65 0.49 0.52 0.59 0.55 0.53 0.35
-# UCSize  0.64   1.00    0.91 0.71 0.75 0.69 0.76 0.72 0.46
-# UCShape 0.65   0.91    1.00 0.69 0.72 0.71 0.74 0.72 0.44
-# MA      0.49   0.71    0.69 1.00 0.59 0.67 0.67 0.60 0.42
-# SECS    0.52   0.75    0.72 0.59 1.00 0.59 0.62 0.63 0.48
-# BN      0.59   0.69    0.71 0.67 0.59 1.00 0.68 0.58 0.34
-# BC      0.55   0.76    0.74 0.67 0.62 0.68 1.00 0.67 0.35
-# NN      0.53   0.72    0.72 0.60 0.63 0.58 0.67 1.00 0.43
-# M       0.35   0.46    0.44 0.42 0.48 0.34 0.35 0.43 1.00
+#            shape size shape.1 adhesion epithelial nuclei chromatin nucleoli mitoses
+# shape       1.00 0.64    0.65     0.49       0.52   0.59      0.55     0.53    0.35
+# size        0.64 1.00    0.91     0.71       0.75   0.69      0.76     0.72    0.46
+# shape.1     0.65 0.91    1.00     0.69       0.72   0.71      0.74     0.72    0.44
+# adhesion    0.49 0.71    0.69     1.00       0.59   0.67      0.67     0.60    0.42
+# epithelial  0.52 0.75    0.72     0.59       1.00   0.59      0.62     0.63    0.48
+# nuclei      0.59 0.69    0.71     0.67       0.59   1.00      0.68     0.58    0.34
+# chromatin   0.55 0.76    0.74     0.67       0.62   0.68      1.00     0.67    0.35
+# nucleoli    0.53 0.72    0.72     0.60       0.63   0.58      0.67     1.00    0.43
+# mitoses     0.35 0.46    0.44     0.42       0.48   0.34      0.35     0.43    1.00
 
-# Como es posible observar, la correlación entre UCSize y UCShape es igual a 0.91,
+# Como es posible observar, la correlación entre size y shape es igual a 0.91,
 # lo que nos indica que estas variables entregan una información muy similar.
 # Debido a esto, se procede a eliminar ese atributo y proceder con el clustering
 # con los 8 atributos restantes.
@@ -157,5 +177,4 @@ plot(modelo,what = "classification")
 #m = cor(data.3)
 #corrplot(m,method="number")
 
-#coef de variacion desv est/media
 
